6d0f1c61f6f493f77ec3aa65b803361eefe4b613
Takeshi Shimmura <tshimmura@tshimmura-ltm2.internal.salesforce.com>
2019-08-07 20:25:07 +0900

    first commit

---
+++ public/js/customActivity.js
@@ -0,0 +1,82 @@
+define([
+    'postmonger'
+], function(
+    Postmonger
+) {
+    'use strict';
+
+    var connection = new Postmonger.Session();
+    var payload = {};
+
+    $(window).ready(onRender);
+
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
+    connection.on('requestedSchema', onGetSchema);
+    connection.on('requestedCulture', onGetCulture);
+
+    connection.on('clickedNext', onClickedNext);
+    connection.on('clickedBack', onClickedBack);
+    connection.on('gotoStep', onGotoStep);
+
+    function onRender() {
+        connection.trigger('ready'); // JB will respond the first time 'ready' is called with 'initActivity'
+
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+        connection.trigger('requestSchema');
+        connection.trigger('requestCulture');
+
+
+    }
+
+    function initialize (data) {
+        if (data) {
+            payload = data;
+        }
+    }
+
+    function onGetTokens (tokens) {
+        // Response: tokens == { token: <legacy token>, fuel2token: <fuel api token> }
+        // console.log(tokens);
+    }
+
+    function onGetEndpoints (endpoints) {
+        // Response: endpoints == { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+        // console.log(endpoints);
+    }
+
+    function onGetSchema (payload) {
+        // Response: payload == { schema: [ ... ] };
+        // console.log('requestedSchema payload = ' + JSON.stringify(payload, null, 2));
+    }
+
+    function onGetCulture (culture) {
+        // Response: culture == 'en-US'; culture == 'de-DE'; culture == 'fr'; etc.
+        // console.log('requestedCulture culture = ' + JSON.stringify(culture, null, 2));
+    }
+
+    function onClickedNext () {
+        save();
+    }
+
+    function onClickedBack () {
+        connection.trigger('prevStep');
+    }
+
+    function onGotoStep (step) {
+        showStep(step);
+        connection.trigger('ready');
+    }
+
+    function showStep(step, stepIndex) {
+    }
+
+    function save() {
+        payload['metaData'].isConfigured = true;
+
+        connection.trigger('updateActivity', payload);
+    }
+
+});
